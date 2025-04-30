e42ff8dbc442045d68cdbcf833a7605b73089b9d
Dulcelene Machado <dudutola@gmail.com>
2025-04-30 15:05:03 +0100

    Added steps#new/create to create new steps Nested routes new/creaye under sections Added partial form to create steps and updated sections#show view with link to create step

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   resources :sections, only: [ :show, :new, :create, :edit, :update, :destroy ] do
     resources :issues, only: [ :show, :new, :create, :edit, :update, :destroy ]
-    resources :steps, only: [ :index, :show ]
+    resources :steps, only: [ :show, :new, :create ]
   end
 
   resources :steps, only: [ :index ]
